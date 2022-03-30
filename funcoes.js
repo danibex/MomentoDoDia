d7902fb27ad8071350f608e93bc1b9cdce47ffa7
danibex <danielivam96@gmail.com>
2022-03-29 21:13:55 -0300

    Finalizando projeto

--- funcoes.js
+++ funcoes.js
@@ -1,22 +1,29 @@
-        let textoHoraDom = document.getElementById('hora')
-        let hora = new Date()
-        let horaTratada = hora.getHours()
-        let min = hora.getMinutes()
-        let momento = document.getElementById('momentoDoDia')
-        let imagem = document.getElementById('imagem-da-hora')
-        let fundo = document.getElementById("cor-de-fundo")
-        textoHoraDom.innerHTML = `Agora são ${horaTratada} : ${min}`
+ function carregar () { 
+var momento = document.getElementById('momento')
+var corpo = document.getElementById('corpo')
+var msg = document.getElementById('msg')
+var foto = document.getElementById('foto')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 23
+msg.innerHTML = `Agora são ${hora} horas.`
 
-        if (horaTratada > 18 || horaTratada < 0) {
-            momento.innerHTML = 'Agora é noite!!!'
-            imagem.innerHTML = '<img src="https://agenciabrasil.ebc.com.br/sites/default/files/atoms/image/926212-super%20lua_1.jpg" alt="foto-noite">'
-            fundo.style.background = '#363636'
-        } else if (horaTratada > 0 || horaTratada < 6) {
-            momento.innerHTML = 'Agora é madrugada!!!'
-            imagem.innerHTML = '<img src="https://www.teclasap.com.br/wp-content/uploads/2015/02/madrugada-2.jpg" alt="foto-madrugada">'
-            fundo.style.background = '#363636'
-        } else if (horaTratada > 6 || horaTratada < 12) {
-            momento.innerHTML = 'Agora é manhã!!!'
-            imagem.innerHTML = '<img src="https://i1.wp.com/nerdizmo.uai.com.br/wp-content/uploads/sites/29/2014/11/morning.jpg?ssl=1" alt="foto-manha">'
-        }
-      
+if (hora >= 0 && hora < 12) {
+    foto.innerHTML = '<img src="fotos/manha.jpg">'
+    corpo.style.background = '#A1BAD5'
+    momento.innerHTML = '<p>Agora é manhã!</p>'
+    //DIA
+} else if (hora >= 12 && hora <= 18) {
+    foto.innerHTML = '<img src="fotos/tarde.jpg">'
+    document.body.style.background = '#B7826D'
+    momento.innerHTML = '<p>Agora é tarde!</p>'
+    //TARDE
+} else {
+    foto.innerHTML = '<img src="fotos/noite.jpg">'
+    document.body.style.background = '#283544'
+    momento.innerHTML = '<p>Agora é noite!</p>'
+    //NOITE
+}
+
+
+ } 
